4c7205574a17b52c498ebf41bdb440e74e01a5f3
juanMoleroD <juanldimeco@gmail.com>
2022-10-25 17:23:16 +0100

    removed incorrect simplebuild and added correct simplebuild-jshint

--- Jakefile.js
+++ Jakefile.js
@@ -2,6 +2,7 @@
     "use strict";
 
     const semver = require("semver");
+    const jshint = require("simplebuild-jshint");
     
     // makes this selfdocumenting
     desc("Description of the task"); 
@@ -27,8 +28,14 @@
     desc("Lint the code");
     task("lint", function() {
         console.log("Linting Js: .");
+        // jshint.checkFiles({
+        //     files: "Jakefile.js",
+        //     options: {},
+        //     globals: {}
+        // }, complete, fail); // complete is async
+        
         // config to run linting, but this is a fallback, this is slower
         jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", { interactive: true }, complete);
-    });
+    }, { async: true });
 
 } ());
